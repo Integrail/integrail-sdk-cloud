4faf8492f265c7728fd5cbf7b3100c3c35f27b53
Daniil Kolesnichenko <garlicbreadcleric@yahoo.com>
2024-09-19 17:10:36 +0400

    Re-export cloud memory api

--- src/api/cloud/cloud.api.ts
+++ src/api/cloud/cloud.api.ts
@@ -5,6 +5,7 @@ import { CloudMemoryApi } from "./cloud-memory.api";
 
 export * from "./cloud-agent.api";
 export * from "./cloud-execution.api";
+export * from "./cloud-memory.api";
 
 export class IntegrailCloudApi extends BaseApi {
   public readonly agent: CloudAgentApi = new CloudAgentApi(this.options);
