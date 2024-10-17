ca26a5bc4c95d697366bf280acebc149ae4b01a5
Daniil Kolesnichenko <garlicbreadcleric@gmail.com>
2024-10-17 07:46:11 +0400

    Add executionId to events

--- src/types/execution.type.ts
+++ src/types/execution.type.ts
@@ -24,6 +24,7 @@ export enum AgentExecutionStatus {
 }
 
 export const BaseEventSchema = z.object({
+  executionId: ExecutionIdSchema,
   createdAt: z.coerce.date(),
 });
 
@@ -266,6 +267,7 @@ export namespace AgentExecution {
   }
 
   export function applyEvent(execution: AgentExecution, event: ExecutionEvent): AgentExecution {
+    if (execution._id !== event.executionId) return execution;
     switch (event.op) {
       case ExecutionEventOp.INIT:
         return init(event);
