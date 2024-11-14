5d745dc1a4c4a0dd42bebf3693fdb8a7aa874535
Daniil Kolesnichenko <garlicbreadcleric@gmail.com>
2024-11-14 20:49:22 +0400

    Add more audio/video content types

--- src/types/content.type.ts
+++ src/types/content.type.ts
@@ -7,20 +7,26 @@ export enum ContentCategory {
   THREE_DIMENSIONAL = "three-dimensional",
 }
 
-export const ContentTypeEnum = Enum.of<"name", { category: ContentCategory }>().create({
+export const ContentTypeEnum = Enum.of<
+  "name",
+  { category: ContentCategory; aliases?: string[] }
+>().create({
   // Image.
   JPEG: { name: "image/jpeg", category: ContentCategory.IMAGE },
 
   // Audio.
   AAC: { name: "audio/aac", category: ContentCategory.AUDIO },
   FLAC: { name: "audio/flac", category: ContentCategory.AUDIO },
-  MP3: { name: "audio/mp3", category: ContentCategory.AUDIO },
+  MP3: { name: "audio/mpeg", category: ContentCategory.AUDIO, aliases: ["audio/mp3"] },
+  M4A: { name: "audio/mp4", category: ContentCategory.AUDIO, aliases: ["audio/m4a"] },
   OPUS: { name: "audio/opus", category: ContentCategory.AUDIO },
   PCM: { name: "audio/pcm", category: ContentCategory.AUDIO },
   WAV: { name: "audio/wav", category: ContentCategory.AUDIO },
+  WEBA: { name: "audio/webm", category: ContentCategory.AUDIO },
 
   // Video.
   MP4: { name: "video/mp4", category: ContentCategory.VIDEO },
+  WEMB: { name: "video/webm", category: ContentCategory.VIDEO },
 
   // 3D.
   GLB: { name: "model/gltf-binary", category: ContentCategory.THREE_DIMENSIONAL },
