ca4fe128b42c915e81a1088851e70cace3a4f759
Daniil Kolesnichenko <garlicbreadcleric@gmail.com>
2024-11-14 23:15:57 +0400

    Add optional contentType field to file/media types

--- src/types/media.type.ts
+++ src/types/media.type.ts
@@ -3,28 +3,35 @@ import { z } from "@/prelude/zod";
 import { TypeName } from "./data.type";
 
 export const ImageSchema = z
-  .object({ type: z.literal(TypeName.IMAGE) })
+  .object({ type: z.literal(TypeName.IMAGE), contentType: z.string().optional() })
   .and(z.union([z.object({ url: z.string().url() }), z.object({ base64: z.string() })]));
 export type Image = z.infer<typeof ImageSchema>;
 
 export const AudioSchema = z.object({
   type: z.literal(TypeName.AUDIO),
   url: z.string().url(),
+  contentType: z.string().optional(),
 });
 export type Audio = z.infer<typeof AudioSchema>;
 
-export const VideoSchema = z.object({ type: z.literal(TypeName.VIDEO), url: z.string().url() });
+export const VideoSchema = z.object({
+  type: z.literal(TypeName.VIDEO),
+  url: z.string().url(),
+  contentType: z.string().optional(),
+});
 export type Video = z.infer<typeof VideoSchema>;
 
 export const FileSchema = z.object({
   type: z.literal(TypeName.FILE),
   url: z.string().url(),
   fileName: z.string(),
+  contentType: z.string().optional(),
 });
 export type File = z.infer<typeof FileSchema>;
 
 export const ThreeDSchema = z.object({
   type: z.literal(TypeName.THREE_DIMENSIONAL),
   url: z.string().url(),
+  contentType: z.string().optional(),
 });
 export type ThreeD = z.infer<typeof ThreeDSchema>;
