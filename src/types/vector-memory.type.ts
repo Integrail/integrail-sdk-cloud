67f3ff80133cc2a57a50fbb66895b26476d1a727
Daniil Kolesnichenko <garlicbreadcleric@gmail.com>
2025-02-06 16:29:03 +0400

    Add metadata to VectorMemory schema

--- src/types/vector-memory.type.ts
+++ src/types/vector-memory.type.ts
@@ -1,5 +1,20 @@
 import { z } from "@/prelude/zod";
 
+import { TypeName } from "./data.type";
+
+export const MemoryMetadataType = z.enum([
+  TypeName.STRING,
+  TypeName.NUMBER,
+  TypeName.DATE,
+  TypeName.BOOLEAN,
+]);
+
+export const MemoryMetadata = z.object({
+  fieldName: z.string(),
+  fieldType: MemoryMetadataType,
+  optional: z.boolean(),
+});
+
 export const VectorMemorySchema = z.object({
   name: z.string(),
   status: z.string(),
@@ -11,5 +26,6 @@ export const VectorMemorySchema = z.object({
   embeddedTextFieldName: z.string(),
   fullTextFieldName: z.string(),
   chunkSize: z.number().int().min(1).nullish(),
+  metadata: z.array(MemoryMetadata).nullish(),
 });
 export type VectorMemory = z.infer<typeof VectorMemorySchema>;
